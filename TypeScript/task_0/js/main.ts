a9377c6af732e3ee4d666bc9dd6045eb77023a6b
OrkhanDervishov <derviorxan7@gmail.com>
2026-05-20 18:40:26 +0400

    main.ts update

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -6,20 +6,20 @@ interface Student {
 }
 
 const student1: Student = {
-  firstName: "John",
-  lastName: "Doe",
+  firstName: 'John',
+  lastName: 'Doe',
   age: 20,
-  location: "New York"
+  location: 'New York',
 };
 
 const student2: Student = {
-  firstName: "Jane",
-  lastName: "Smith",
+  firstName: 'Jane',
+  lastName: 'Smith',
   age: 22,
-  location: "London"
+  location: 'London',
 };
 
-const studentsList: Student[] = [student1, student2];
+const studentsList: Array<Student> = [student1, student2];
 
 const table: HTMLTableElement = document.createElement('table');
 
@@ -32,32 +32,42 @@ const headerRow: HTMLTableRowElement =
 const th1: HTMLTableCellElement =
   document.createElement('th');
 
+th1.textContent = 'First Name';
+
 const th2: HTMLTableCellElement =
   document.createElement('th');
 
+th2.textContent = 'Location';
+
 headerRow.appendChild(th1);
 headerRow.appendChild(th2);
+
 tableHeader.appendChild(headerRow);
 table.appendChild(tableHeader);
 
 const tableBody: HTMLTableSectionElement =
   document.createElement('tbody');
 
-studentsList.forEach((student) => {
+studentsList.forEach((student: Student): void => {
   const row: HTMLTableRowElement =
     document.createElement('tr');
 
   const firstNameCell: HTMLTableCellElement =
     document.createElement('td');
 
+  firstNameCell.textContent = student.firstName;
+
   const locationCell: HTMLTableCellElement =
     document.createElement('td');
-  
+
+  locationCell.textContent = student.location;
+
   row.appendChild(firstNameCell);
   row.appendChild(locationCell);
-  
+
   tableBody.appendChild(row);
 });
 
 table.appendChild(tableBody);
+
 document.body.appendChild(table);
