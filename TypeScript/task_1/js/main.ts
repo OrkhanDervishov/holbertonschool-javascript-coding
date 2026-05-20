bf58855581a69a3352e0847bba0bb4eb239cac02
OrkhanDervishov <derviorxan7@gmail.com>
2026-05-20 18:53:29 +0400

    main.ts update

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -31,10 +31,7 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (
-  firstName: string,
-  lastName: string
-): string => {
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
